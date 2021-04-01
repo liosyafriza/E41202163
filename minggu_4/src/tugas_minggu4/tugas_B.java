41aa7473426b4b0d9aca24de5b763ce313ecf9c5
liosyafriza <80315321+liosyafriza@users.noreply.github.com>
2021-04-01 20:19:10 +0700

    minggu 4

--- minggu_4/src/tugas_minggu4/tugas_B.java
+++ minggu_4/src/tugas_minggu4/tugas_B.java
@@ -10,16 +10,31 @@ package tugas_minggu4;
  * @author ASUS
  */
 public class tugas_B {
+
     public static void main(String[] args) {
         System.out.println("do__while");
-        System.out.println("bilangan kelipatan 2(1-100)");
+        System.out.println(" kelipatan 2(0-100)");
         System.out.println("===========================");
-    int i = 1;
-    do{
-        System.out.print(i + " ");
-    i = i*2;
-    }while (i<=100);
+        int i = 0;
+        do {
+            if (i == 0) {
+                i = 1;
+            } else {
+                System.out.println(i + " ");
+            }
+            i = i * 2;
+        } while (i <= 100);
+        System.out.println("");
+        System.out.println("Bilangan [perkalian 2 {0-100)");
+        System.out.println("===============================");
+        int j = 0;
+        do {
+            if (j == 0) {
+                j = 1;
+            } else {
+                System.out.println(j * 2 + "");
+            }
+            j++;
+        } while(j <= 100);
     }
 }
-    
-
